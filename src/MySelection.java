e7296cd674b633773d85d6f9836da6ba0360b3e9
Matthew Laten <matt.laten@gmail.com>
2013-03-14 12:37:20 +0200

    Implemented deterministic tournament selection. Best is about 21k

--- src/MySelection.java
+++ src/MySelection.java
@@ -1,15 +1,38 @@
+import java.util.Arrays;
+
 public class MySelection extends Selection {
 	int popsize;
-
+	
+	//tourney selections
+	
+	int k = 3;
+	
 	public void initialize(int popsize) {
 		this.popsize = popsize;
 	}
 
 	public Individual[] select(Individual[] population) {
+		
 		Individual[] newPopulation = new Individual[popsize];
 
-		// Code for your selection operator ...
+		for (int i = 0; i < popsize; i++){
+			//create set, choose winner, add to new pop
+			Individual [] tournamentSet = new Individual[k];
+			for (int j = 0; j < k; j++) {
+				tournamentSet[j] = population[(int) Math.floor(Math.random()*popsize)];
+			}
+			
+			Arrays.sort(tournamentSet);
 
+			//System.out.println(tournamentSet[0].getFitness() + " vs " + tournamentSet[k-1].getFitness());
+			newPopulation[i] = tournamentSet[0];
+		}
+		
+		// Code for your selection operator ...
+		
 		return newPopulation;
 	}
+	
+	
+	
 }
