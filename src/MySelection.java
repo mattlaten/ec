bb6747b3a7ee8229e0aa63b68816d6d124b68660
Matthew Laten <matt.laten@gmail.com>
2013-03-18 16:43:04 +0200

    Implemented WeightedRandom comparator. Implemented tournament winner removal

--- src/MySelection.java
+++ src/MySelection.java
@@ -1,38 +1,85 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class MySelection extends Selection {
 	int popsize;
 	
 	//tourney selections
 	
-	int k = 3;
+	int k = 11;
+	double percentParents = 0.9;
+
 	
 	public void initialize(int popsize) {
 		this.popsize = popsize;
 	}
 
 	public Individual[] select(Individual[] population) {
+
+		int numParents = (int) Math.floor(percentParents*popsize);
 		
 		Individual[] newPopulation = new Individual[popsize];
 
-		for (int i = 0; i < popsize; i++){
+		ArrayList<Individual> popList = new ArrayList<Individual>(Arrays.asList(population));
+		ArrayList<Individual> newPopList = new ArrayList<Individual>(popsize);
+		
+		for (int i = 0; i < numParents; i++){
 			//create set, choose winner, add to new pop
-			Individual [] tournamentSet = new Individual[k];
-			for (int j = 0; j < k; j++) {
-				tournamentSet[j] = population[(int) Math.floor(Math.random()*popsize)];
+			if (popList.size() > k) {
+				Individual [] tournamentSet = new Individual[k];
+				for (int j = 0; j < k; j++) {
+					tournamentSet[j] = popList.remove((int) Math.floor(Math.random()*popList.size()));
+				}
+				
+				Arrays.sort(tournamentSet, new WeightedRandom());
+	
+				//System.out.println(tournamentSet[0].getFitness() + " vs " + tournamentSet[k-1].getFitness());
+				//System.out.println("Adding winner from tournamentSet: " + tournamentSet[0]);
+				newPopList.add(tournamentSet[0]);
+				
+				for (int j = 1; j < k; j++) {
+					popList.add(tournamentSet[j]);
+				}
+			}
+			else {
+				numParents = i+1;
+				break;
 			}
 			
-			Arrays.sort(tournamentSet);
-
-			//System.out.println(tournamentSet[0].getFitness() + " vs " + tournamentSet[k-1].getFitness());
-			newPopulation[i] = tournamentSet[0];
+		}
+		
+		int count = 0;
+		for (Individual i : newPopList) {
+			//System.out.println(count + " derp:" + i);
+			++count;
+		}
+		
+		//System.out.println(numParents + " vs " + popsize);
+		while (newPopList.size() < popsize) {
+			newPopList.add(newPopList.get((int) Math.floor(Math.random()*numParents)));
+			//System.out.println(" herp:" + newPopList.size());
 		}
 		
 		// Code for your selection operator ...
-		
+		newPopList.toArray(newPopulation);
 		return newPopulation;
 	}
 	
-	
+	private class WeightedRandom implements Comparator<Individual>
+	{
+
+		@Override
+		public int compare(Individual i, Individual o) {
+			// TODO Auto-generated method stub
+			double weight1 = Math.random();
+			double weight2 = Math.random();
+			if (i.getFitness()*weight1 < o.getFitness()*weight2) 
+			    return -1;
+			else if (i.getFitness()*weight1 > o.getFitness()*weight2)
+			    return 1;
+			return 0;
+		}
+	}
 	
 }
